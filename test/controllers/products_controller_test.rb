28ceb3ed5d4c8830aa61f91b5e23e954601d661a
Hernan <hernan.bordon@gmail.com>
2020-05-04 16:09:15 -0300

    - Add quantity colum to LineItems - Migrating existing data to the new table format - Implementing flash messaging for errors - Removing unnecessary parameters for line_item - Adjust the cart view

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -41,7 +41,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "can't delete product in cart" do
     assert_difference('Product.count', 0) do
-      delete product_url(product(:two))
+      delete product_url(products(:two))
     end
 
     assert_redirected_to products_url
