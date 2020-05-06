58b2f022d9dc896c76f99642c3558e02ddf652a6
Hernan <hernan.bordon@gmail.com>
2020-05-05 22:35:42 -0300

    - Add AJAX functionality to load items to the cart - Add js for hide the cart when is empty - Use Ajax to remove the flash message when the cart have an item

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -47,6 +47,18 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       delete line_item_url(@line_item)
     end
 
-    assert_redirected_to line_items_url
+    assert_redirected_to cart_url(@line_item.cart)
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id },
+        xhr: true
+    end 
+
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body
+    puts "<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>"
+    puts @response.body
   end
 end
